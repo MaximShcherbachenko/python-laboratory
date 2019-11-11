24f9d3ca3d879b886aba8d8213d5eb6d26974031
MaximShcherbachenko <55317211+MaximShcherbachenko@users.noreply.github.com>
2019-11-11 20:16:47 +0200

    Update task1.py

--- laboratory1/task1.py
+++ laboratory1/task1.py
@@ -3,6 +3,8 @@ import re
 
 re_integer = re.compile("\d+$")
 
+re_integer = re.compile("\d+$")
+
 def validator_1(pattern, promt):
     a_value = input(promt)
     while not bool(pattern.match(a_value)):
@@ -14,6 +16,10 @@ def validator_2(prompt):
     number = float(validator_1(re_integer, prompt))
     return number
 
-a = validator_2("Enter value of a:")
-b = validator_2("Enter value of b:")
-print((a + 4 * b) * (a - 3 * b) + 2 * a)
+while True:
+    a = validator_2("Enter value of a:")
+    b = validator_2("Enter value of b:")
+    print((a + 4 * b) * (a - 3 * b) + 2 * a)
+    q = input("Press <<->> for exit or any button to continue:")
+    if q == "-":
+        break
