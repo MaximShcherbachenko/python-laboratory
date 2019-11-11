85e6b89e2f3067ab8f57ed59964fc1feb0bf6b13
MaximShcherbachenko <55317211+MaximShcherbachenko@users.noreply.github.com>
2019-11-11 20:24:00 +0200

    Add files via upload

---
+++ laboratory1/Lab1.3.py
@@ -0,0 +1,37 @@
+"""F(x) = {(-x^2 if x>=7) or (2^(x)/(x^2 - 9))}"""
+import re
+
+re_integer = re.compile("\d+$")
+
+def validator_1(pattern, promt):
+    a_value = input(promt)
+    while not bool(pattern.match(a_value)):
+        a_value = input(promt)
+    return a_value
+
+
+def validator_2(prompt):
+    number = float(validator_1(re_integer, prompt))
+    return number
+
+while True:
+    x = validator_2("Enter variable x:")
+
+
+    def variations(value):
+        if value > 7:
+            print(-value ** 2)
+        elif value <= 7:
+            while value == 3 or value == -3:
+                value = validator_2("Enter variable x:")
+            if value == 7:
+                print(-value ** 2)
+                print((2 ** (-value)) / (value ** 2 - 9))
+        else:
+            print((2 ** (-value)) / (value ** 2 - 9))
+
+
+    variations(x)
+    q = input("Press <<->> for exit or any button(s) to continue:")
+    if q == "-":
+        break
