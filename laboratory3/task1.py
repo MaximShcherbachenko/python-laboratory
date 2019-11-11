1bc9375cd531150806ddc47365ef5e4e42f42b70
MaximShcherbachenko <55317211+MaximShcherbachenko@users.noreply.github.com>
2019-11-11 21:40:20 +0200

    Update task1.py

--- laboratory3/task1.py
+++ laboratory3/task1.py
@@ -1,15 +1,38 @@
 """З рядка видалити всі цифри і малі букви."""
+import re
 
-"""Видалити з рядка всі цифри і маленькі букви за допомогою змінної string, у яку буде записуватися рядок, введений користувачем
 q = 0
+
+re_integer = re.compile("\w+$")
+
+def validator_1(pattern, promt):
+    value = input(promt)
+    while not bool(pattern.match(value)):
+        value = input(promt)
+    return value
+
+
+def validator_2(prompt):
+    string = str(validator_1(re_integer, prompt))
+    return string
+
+our_string = validator_2("Enter string:")
+
+
+def deleting_incorrect_symbols():
+    global our_string
+    for i in range(0, len(our_string)):
+        if our_string[i].islower() or our_string[i].isdigit():
+            our_string = our_string[:i] + " " + our_string[i + 1:]
+    return str(our_string)
+
 while q != "-":
-    string = input("Enter string:\n")
-    for i in range(0, len(string)):
-        if string[i].islower() or string[i].isdigit():
-            string = string[:i] + " " + string[i + 1:]
-    if string.replace(" ", "") == string:
-        print(string, "- result")
+    deleting_incorrect_symbols()
+    if our_string.replace(" ", "") == our_string:
+        print(our_string, "- result")
     else:
-        print(string, "- for clarity")
-        print(string.replace(" ", ""), "- without spaces")
+        print(our_string, "- for clarity")
+        print(our_string.replace(" ", ""), "- without spaces")
     q = input("Press <<->> for exit or any button(s) to continue:")
+    if q == "-":
+        break
